b8afaf4dd078f2a797eddfb892128fcd882da517
Rynex Native <rynexnative@gmail.com>
2025-07-29 13:58:54 +0300

    fist from local

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/rynexnative-digital-vision/", // ← Badilisha jina kulingana na repo yako
+  base: "/", // ← Badilisha jina kulingana na repo yako
   // server: {
   //   host: "::",
   //   port: 8080,
